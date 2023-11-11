4c41f8b6b2deacd2182031547afdb10389721a5a
angelalita77 <anjeanette@gmail.com>
2023-11-11 18:26:43 -0500

    passsed the first jest test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,12 @@
-const repeatString = function() {
-
-    let singleString = "hey";
-
-    while (i=0, i++, i<3)
-    {
-        
-    }
-
+const repeatString = function(word, num) {
+	let str = '';
+for (let i=0; i<num; i++) {
+	str += word;
+}
+return str;
 };
+
+console.log(repeatString('hey',3));
 
 // Do not edit below this line
 module.exports = repeatString;
