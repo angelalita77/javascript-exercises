a385d4bfae9df273cbb68bceb997f9a28f4d0f06
angelalita77 <anjeanette@gmail.com>
2023-11-12 20:56:41 -0500

    Passed the 5th test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,15 +1,24 @@
 const repeatString = function(word, num) {
 	let str = '';
-for (let i=0; i<num; i++) {
-	str += word;
-}
-return str;
+
+    if (num >= 0)
+    {
+        for (let i=0; i<num; i++) {
+            str += word;
+        }
+        return str;
+    }
+    else {
+      return "ERROR";
+    }
+
 };
 
 console.log(repeatString('hey',3));
 console.log(repeatString('hey',10));
-//console.log(repeatString('hey',1));
-//console.log(repeatString('hey',1));
+console.log(repeatString('hey',1));
+console.log(repeatString('hey',0));
+console.log(repeatString('hey',-1));
 
 // Do not edit below this line
 module.exports = repeatString;
