4f361c44635b5f464196a1a3b89ed43ce54aa848
angelalita77 <anjeanette@gmail.com>
2024-01-15 16:46:58 -0500

    Completed second test with mulitple elements to remove

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,10 @@
-const removeFromArray = function(anArray, removeElement) {
-    let newArray = anArray.filter(item => item !== removeElement);
+const removeFromArray = function(anArray, ...removeElement) {
+    let newArray = anArray.filter(item => !removeElement.includes(item));
     return newArray;
 };
 
 console.log(removeFromArray([1, 2, 3, 4], 3));
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
