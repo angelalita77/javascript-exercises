f7f67d95815a5f0cc067f2ae062cf54d109ce880
angelalita77 <anjeanette@gmail.com>
2024-01-15 14:48:22 -0500

    Passed and Failed the first test using filter method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(anArray, removeElement) {
+    let newArray = anArray.filter(item => item !== removeElement);
+    return newArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
