3ecacba9004d88582cb3a21ee3ca94b175502f6f
angelalita77 <anjeanette@gmail.com>
2024-01-15 11:30:43 -0500

    Completed three test using reverse, join and split

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
+const reverseString = function(str) {
+
+let splitString = str.split("");
+console.log(splitString);
+
+let reverseString = splitString.reverse();
+console.log(reverseString);
+
+let joinString = reverseString.join('');
+console.log(joinString);
+return joinString;
+
 
 };
+
+console.log(reverseString("hello"));
+console.log(reverseString("hello there"));
+console.log(reverseString('123! abc!'));
+console.log(reverseString(''));
 
 // Do not edit below this line
 module.exports = reverseString;
