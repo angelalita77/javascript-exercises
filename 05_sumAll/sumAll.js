d39166e4101f9367775f468fe0785b1ee3b377a4
angelalita77 <anjeanette@gmail.com>
2024-05-05 17:54:42 -0400

    Added func arguments and loop for first 3 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+const sumAll = function(start, end) {
+    let sum = 0;
+      if (start >  end)
+        {  for (let i = end; i <= start; i++) {
+          sum += i; }
+        }
+      else
+      {
+      for (let i = start; i <= end; i++) {
+        sum += i;
+        }
+      }
+  return sum;
+  }
+  
+  console.log(sumAll(1, 4));
+  console.log(sumAll(1, 4000));
+  console.log(sumAll(123, 1));
+ 
+  
 
 // Do not edit below this line
 module.exports = sumAll;
