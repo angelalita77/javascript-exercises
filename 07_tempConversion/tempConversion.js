092a69efd7aa519dde586382f68b5c45755a282e
angelalita77 <anjeanette@gmail.com>
2024-05-15 21:13:44 -0400

    Completed test 1 and 2

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  return console.log(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * (9 / 5)) + 32;
+  return console.log(fahrenheit.toFixed(1));
 };
+
+convertToCelsius(32);
+convertToCelsius(100);
+
 
 // Do not edit below this line
 module.exports = {
