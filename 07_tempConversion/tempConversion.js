08fb35828edf66012712c5ccfd4df43fc6317f20
angelalita77 <anjeanette@gmail.com>
2024-05-19 22:57:56 -0400

    Duplicated the conditionals from convertToCelsius in convertToFahrenheit. This time the dta=s

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -11,12 +11,22 @@ const convertToCelsius = function(fahrenheit) {
 
 const convertToFahrenheit = function(celsius) {
   let fahrenheit = (celsius * (9 / 5)) + 32;
+// subtract whole number minus decimaled number. If it's not zero
+// with a decimal the difference,  that mean celsius was a decimal number.
+  if (Math.trunc(fahrenheit) - fahrenheit !== 0) {
+    return Math.round(fahrenheit * 10) / 10; }
+    else {
   return fahrenheit;
+  }
 };
 
 console.log(convertToCelsius(32));
 console.log(convertToCelsius(100));
 console.log(convertToCelsius(-100));
+
+console.log(convertToFahrenheit(0));
+console.log(convertToFahrenheit(73.2));
+console.log(convertToFahrenheit(-10));
 
 
 // Do not edit below this line
